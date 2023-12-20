21ff6c591e1a093efad4b392490016b7c54635c9
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-12-20 21:48:23 +0900

    idk

--- serve.ts
+++ serve.ts
@@ -6,18 +6,16 @@ console.log("About me: https://ringoxd.pages.dev/");
 
 const app = express();
 
-app.use((req) => {
+
+app.get("/", (req, res) => {
     const now = new Date();
+    console.log(req)
     const clientIP = req.ip; // クライアントのIPを取得
     const requestInfo = `${req.method} ${decodeURIComponent(req.originalUrl)}`; // リクエストのメソッドとURL
 	const userAgent = req.headers['user-agent'];
 	console.log(userAgent)
     console.log(`[${now.toLocaleString()}] - Client IP: ${clientIP}, Request: ${requestInfo}`);
-
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.listen(8000);

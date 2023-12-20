7302818b507197abc08db77ea82ac5a8ef3bd0ee
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-12-20 21:45:32 +0900

    ignore this error (lol)

--- serve.ts
+++ serve.ts
@@ -1,21 +1,24 @@
 // @deno-types="npm:@types/express@4.17.15"
 import express from "npm:express@4.18.2";
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 console.log("Created by ringoXD.");
 console.log("About me: https://ringoxd.pages.dev/");
 
+const app = express();
 
-function handler(req: Request): Response {
+app.use((req, next) => {
     const now = new Date();
-    console.log(req)
     const clientIP = req.ip; // クライアントのIPを取得
     const requestInfo = `${req.method} ${decodeURIComponent(req.originalUrl)}`; // リクエストのメソッドとURL
 	const userAgent = req.headers['user-agent'];
 	console.log(userAgent)
     console.log(`[${now.toLocaleString()}] - Client IP: ${clientIP}, Request: ${requestInfo}`);
-    new Response("Hello world"); //*Hello World!
-    return new Response("Hello, World");
-}
 
-serve(handler);
+    next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(8000);
